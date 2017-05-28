e3444e633d68c31b4fb1fb60ecfe163b5a364c7b
malikehsan <ehsan.ehsanullah@carleton.ca>
2017-05-28 14:37:15 -0400

    Update functions.js

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -52,7 +52,7 @@ function  workLoad() {
     var $this = $(this),
         newTitle = $this.find('strong').text(),
         spinner = '<div class="loader">Loading...</div>',
-        url = $this.find('.thumb-unit').data('url')+ ".html";
+        url = $this.find('.thumb-unit')+ ".html";
 
     $('.project-load').html(spinner).text(url);
     $('.project-title').text(newTitle);
